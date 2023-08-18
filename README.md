f4a277e21047560e3274e7e42dc5d2f8cb38c097
gvgvli <gvgvli@tencent.com>
2023-08-18 18:19:12 +0800

    feat: 🎸 init

---
+++ README.md
@@ -0,0 +1,1 @@
+# mobx-miniprogram-lite
---
+++ scripts/build.js
@@ -0,0 +1,62 @@
+const esbuild = require('esbuild')
+const { join, resolve } = require('path')
+
+const shouldWatch =
+  process.argv.length > 2 && ['-w', '--watch'].includes(process.argv[2])
+
+;(async () => {
+  const pkgPath = join(resolve(__dirname, '..', 'package.json'))
+  const pkgInfo = require(pkgPath)
+  const basePath = (() => {
+    return resolve(pkgPath, '..')
+  })()
+
+  const entryPoints = join(basePath, 'src', 'index.ts')
+
+  const outDir = join(basePath, 'lib')
+
+  const peerDependencyNames = pkgInfo.peerDependencies
+    ? Object.keys(pkgInfo.peerDependencies)
+    : []
+
+  /** @type {import('esbuild').BuildContext} */
+  const esmCtx = await esbuild.context({
+    entryPoints: [entryPoints],
+    outdir: outDir,
+    bundle: true,
+    target: 'es2015',
+    sourcemap: true,
+    external: peerDependencyNames,
+    splitting: true,
+    format: 'esm',
+    tsconfig: join(basePath, 'tsconfig.json'),
+  })
+
+  /** @type {import('esbuild').BuildContext} */
+  const cjsCtx = await esbuild.context({
+    entryPoints: {
+      ['index.common']: entryPoints
+    },
+    outdir: outDir,
+    bundle: true,
+    platform: 'node',
+    target: 'es2015',
+    sourcemap: true,
+    external: peerDependencyNames,
+    format: 'cjs',
+    tsconfig: join(basePath, 'tsconfig.json'),
+  })
+
+  if (shouldWatch) {
+    esmCtx.watch()
+    cjsCtx.watch()
+  } else {
+    await Promise.all([
+      esmCtx.rebuild(),
+      cjsCtx.rebuild(),
+    ]).finally(() => {
+      esmCtx.dispose()
+      cjsCtx.dispose()
+    })
+  }
+})()
